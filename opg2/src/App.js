c7325fdca4a7fd698a2bde76eb1f2123badaff20
Lars <lars.moster@hotmail.com>
2018-09-10 12:19:19 +0200

    add basic fetch skeleton

--- opg2/src/App.js
+++ opg2/src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import beach_boys from '../public/text/beach_boys';
 
 class App extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          error : null,
+          isLoaded: false,
+          items: null
+      };
+  }
+    componentDidMount() {
+      fetch ('text/beach_boys.json')
+          .then(res => res.json())
+          .then(
+              (result) => {
+                  this.setState({
+                      isLoaded: true,
+                      items: result
+                  });
+              },
+              (error) => {
+                  this.setState({
+                      isLoaded: true,
+                      error
+                  });
+              }
+          )
+    }
+//beach_boys.songVerses.verse0.join('\n')
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      const {error, isLoaded, items} = this.state;
+      if (error) {
+          return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+          return <div>Loading...</div>;
+      } else {
+          return (
+              <div className="App">
+                  <header className="App-header">
+                      <img src={logo} className="App-logo" alt="logo"/>
+                      <h1 className="App-title">Welcome to React</h1>
+                  </header>
+                  <div id="text">
+                      <p>
+                          {this.state.items.songVerses[0].verse0[0]}
+                          <br />
+                          {this.state.items.songVerses[0].verse0[1]}
+                      </p>
+                  </div>
+              </div>
+          );
+      }
   }
 }
-
+//[]
 export default App;
