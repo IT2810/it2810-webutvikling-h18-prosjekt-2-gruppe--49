cdd01b846d1251c701c2661bd5b5d4db7927a59d
Birger <birgerto@stub.ntnu.no>
2018-09-18 19:26:28 +0200

    add frames for main content #2

--- opg2/src/App.js
+++ opg2/src/App.js
@@ -8,6 +8,18 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Group 49</h1>
         </header>
+        <div className="App-body">
+          <div className="App-content">
+
+            {/* This is where the gallery will be */}
+
+          </div>
+          <div className="App-controllers">
+
+            {/* This is where the image selectors etc. will be */}
+
+          </div>
+        </div>
       </div>
     );
   }
