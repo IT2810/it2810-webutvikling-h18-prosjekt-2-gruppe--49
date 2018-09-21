834ed81f78bc5cb2bde6fe655dcdce73f06463ee
Benjamin Fimreite <benjamfi@stud.ntnu.no>
2018-09-21 17:03:14 +0200

    make changes in controller and display to generate art-displays

--- opg2/src/Controller.js
+++ opg2/src/Controller.js
@@ -1,33 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import _ from 'lodash'
 
 class Controller extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            callBack: props.callBack
+        };
+    }
+
     render() {
         return (
             <div className="App-controller">
-                <RadioGroup name="Image" value1="Air" value2="Land" value3="Sea" />
-                <RadioGroup name="Sound" value1="Wind" value2="Ground" value3="Waves" />
-                <RadioGroup name="Text" value1="Steve Miller Band" value2="Mastodon" value3="Beach Boys" />
+                <RadioGroup callBack={this.state.callBack} name="Image" value1="Air" value2="Earth" value3="Water"/>
+                <RadioGroup callBack={this.state.callBack} name="Sound" value1="Air" value2="Earth" value3="Water"/>
+                <RadioGroup callBack={this.state.callBack} name="Text" value1="Steve Miller Band" value2="Mastodon"
+                            value3="Beach Boys"/>
             </div>
         )
     }
 }
-function RadioGroup (props) {
+
+function RadioGroup(props) {
     return (
         <div className="Controller-RadioGroup">
             <div className="Controller-Header">{props.name}</div>
-            <Radio name={props.name} value={props.value1} />
-            <Radio name={props.name} value={props.value2} />
-            <Radio name={props.name} value={props.value3} />
+            <Radio callBack={props.callBack} name={props.name} value={props.value1}/>
+            <Radio callBack={props.callBack} name={props.name} value={props.value2}/>
+            <Radio callBack={props.callBack} name={props.name} value={props.value3}/>
         </div>
     )
 }
-function Radio (props) {
-    const id = _.uniqueId("prefix-");
 
+function Radio(props) {
+
+
+    const id = _.uniqueId("prefix-");
     return (
         <div>
-            <input id={id} type="radio" name={props.name} value={props.value} />
+            <input onChange={(e) => props.callBack(e.target.name, e.target.value.split(" ").join("_"))} id={id}
+                   type="radio" name={props.name} value={props.value}/>
             <label htmlFor={id}>{props.value}</label>
         </div>
     )
